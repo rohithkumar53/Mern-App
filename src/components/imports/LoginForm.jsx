f0bec706da4aa4278fb445a8571b6f045b3052d3
Rohith Pampari (Contractor) <rohith.pampari@gilead.com>
2021-10-29 14:08:46 +0100

    added icons

--- src/components/imports/LoginForm.jsx
+++ src/components/imports/LoginForm.jsx
@@ -11,9 +11,11 @@ export default function LoginForm(props) {
                   <form method='POST'>
                     <div className="form-group form-box">
                       <input type="text" id='email' className="input-text" placeholder="Email Address" />
+                      <i className='icon email'></i>
                     </div>
                     <div className="form-group form-box">
                       <input type="text" id='password' className="input-text" placeholder="Password" />
+                      <i className='icon lock'></i>
                     </div>
                     <div className="form-group">
                       <button className='btn primary-btn'>Login</button>
