fd8e120e9c5b2397a2a118a7d23137aa266a26cb
Rohith Pampari (Contractor) <rohith.pampari@gilead.com>
2021-11-02 19:35:25 +0000

    user authentication

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -8,7 +8,7 @@ exports.registerUser=async (req,res)=>{
             return res.status(406).json({err:"You have to fill the registration form"});
         }
         let {email, password, passwordCheck, username}= req.body;
-        if(!email || !password || !passwordCheck || !username){
+        if(!email || !password || !passwordCheck){
             return res.status(406).json({err:"Not all fields have been entered"});
         }
         if(password.length<8){
@@ -42,18 +42,28 @@ exports.registerUser=async (req,res)=>{
 
 
 //controller for login
-exports.login= (req, res)=>{
+exports.login= async (req, res)=>{
     try {
         if(!req.body){
             return res.status(406).json({err: "You have to enter the login credentials"});
         }
+        //get user data
         const {email, password}= req.body;
+        //validation
         if(!email || !password){
             return res.status(406).json({err : "Not all fields have been entered"});
         }
-        const user= "$2b$10$WDNpyiWnBhz1q575.R21DOcFbxNA05d6JegvQSIS5R.jVOLGR..mO";
-        const isMatch= bcrypt.compare(password, user);
-        res.json({email,isMatch});
+        const user= await User.findOne({email});
+        if(!user){
+            return res.status(406).json({err: "No account with this email"});
+        }
+
+        //compare password
+        const isMatch= await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(406).json({err: "Invalid credential"});
+        }
+        res.json({username: user.username, email: user.email});
     } catch (error) {
         res.status(500).json({err: error.message || "Error while login!!"});
     }
