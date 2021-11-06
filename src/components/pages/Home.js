0d0fdc883f7d909482f03377ae3747f3db24eccd
Rohith Pampari (Contractor) <rohith.pampari@gilead.com>
2021-11-06 21:46:02 +0000

    home page restricted

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react';
 import Header from '../imports/Header';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 export default function Home() {
+
+    const user = useSelector(state => state.isLoggedIn);
+    const history= useHistory();
+    useEffect(() => {
+        if(!user){
+            history.push("/login");
+        }
+    }, [user, history]);
+
+    console.log(user);
+
     return (
         <>
             <Header/>
