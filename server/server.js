5661fc3fc7e16066b889727dfb2a1981c86dfdd4
rohith_kumar_pampari <rohithkumar.pampari@gmail.com>
2021-11-07 23:14:34 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/api', require('./router/router'));
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../build", "index.html")));
+        res.sendFile(path.join(__dirname, "../build", "index.html"))
     })
 }
 
