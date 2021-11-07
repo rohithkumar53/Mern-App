6737156ab9a050052941d6371cf0e8bf3937a5d3
rohith_kumar_pampari <rohithkumar.pampari@gmail.com>
2021-11-07 22:36:40 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express= require('express');
 //create express app
 const app=express();
 const cors= require("cors");
+const path = require("path");
 require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
-const PORT= process.env.PORT2 || 8080;
+const PORT= process.env.PORT || 8080;
 
 const connect = require("./database/connection");
 
@@ -15,6 +16,13 @@ connect();
 
 //routes
 app.use('/api', require('./router/router'));
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../build", "index.html")));
+    })
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
